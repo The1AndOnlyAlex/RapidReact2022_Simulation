a164034fdb69933785b9b22021c3499e20e1a833
The1AndOnlyAlex <alex.l.duan@gmail.com>
2022-03-06 23:08:07 -0800

    recovery lib

--- src/main/java/com/nerdherd/lib/drivetrain/experimental/Drivetrain.java
+++ src/main/java/com/nerdherd/lib/drivetrain/experimental/Drivetrain.java
@@ -25,10 +25,8 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
-import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.simulation.AnalogGyroSim;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 import edu.wpi.first.math.geometry.Pose2d;
@@ -77,8 +75,7 @@ public class Drivetrain extends AbstractDrivetrain {
 	public Field2d m_field = new Field2d();
 
 	public double simLeftVolt, simRightVolt;
-
-
+	
     public Drivetrain(SmartCANMotorController leftMaster, SmartCANMotorController rightMaster, CANMotorController[] leftSlaves, CANMotorController[] rightSlaves, boolean leftInversion, boolean rightInversion, double trackwidth) {
         m_leftMaster = leftMaster;
 		m_rightMaster = rightMaster;
